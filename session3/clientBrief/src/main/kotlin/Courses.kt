e4fc8e3aee16705a13be0a284989c5776b48ddec
RayanKhyare <71662425+RayanKhyare@users.noreply.github.com>
2021-10-19 09:48:11 +0200

    Made clientBrief for session 3

---
+++ session3/clientBrief/src/main/kotlin/Courses.kt
@@ -0,0 +1,12 @@
+class Course(
+    val courseName: String,
+    val semester: Int,
+    val credits: Int,
+    val teacher : String,
+    val description: String,
+    val jaar : Int,
+    val module: String,
+    val required : Boolean
+) {
+
+}
---
+++ session3/clientBrief/src/main/kotlin/clientBrief.kt
@@ -0,0 +1,7 @@
+fun main(){
+
+    val design1 = Course("Design 1",1,5, "Maaike Beuten", "Designing stuff", 1 , "Design",true);
+    val dev1 = Course("Development 1",1,5, " Peter Dickx", "Developing stuff", 1 , "Development",true);
+    println(dev1.courseName)
+}
+
