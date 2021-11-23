98a97579a0049498fe06f10b7e0f30a6d30740d2
RayanKhyare <71662425+RayanKhyare@users.noreply.github.com>
2021-11-23 11:21:47 +0100

    Connected kotlin to database and tried statement

---
+++ session6/src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+import java.sql.*
+import java.util.*
+
+fun main(){
+    // Create a driver: the library/class that will create the connection
+    Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance()
+
+    val credentials = Credentials()
+
+    // Prepare credentials
+    val connectionProps = Properties()
+    connectionProps["user"] = credentials.user
+    connectionProps["password"] = credentials.password
+
+// Create the connection: this will allow us to run queries on it later
+    val connection =  DriverManager.getConnection(
+        "jdbc:" + "mysql" + "://" +
+                "dt5.ehb.be" +
+                ":" + "3306" + "/" +
+                credentials.databaseName,
+        connectionProps)
+
+    val statement = connection.prepareStatement("SELECT * FROM trains")
+
+    val result = statement.executeQuery()
+
+    println(result)
+}
