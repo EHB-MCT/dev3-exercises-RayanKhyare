575d9f69244c3fb729e1c1a714098e549d73a319
RayanKhyare <71662425+RayanKhyare@users.noreply.github.com>
2021-12-07 10:21:58 +0100

    Made part 2b of S7

--- session7/src/main/kotlin/Duolingo.kt
+++ session7/src/main/kotlin/Duolingo.kt
@@ -2,42 +2,40 @@ open class Duolingo(val roundSize: Int = 5, val language : String) {
 
 
     val words = mutableListOf<Word>(
-        Word("Hello", "Hallo", "en"),
-        Word("Today", "Vandaag", "en"),
-        Word("Food", "Voedsel", "en"),
-        Word("Triangle", "Driehoek", "en"),
-        Word("Bird", "Vogel", "en"),
-        Word("Ghost", "Spook", "en"),
-        Word("Night", "Nacht", "ens"),
-        Word("Shoes", "Schoenen", "en"),
-        Word("Earth", "Aarde", "en"),
-        Word("Jail", "Gevangenis", "en"),
-        Word("Bonjour", "Hallo", "fr"),
-        Word("Aujourd'hui", "Vandaag", "fr"),
-        Word("Nourriture", "Voedsel", "fr"),
-        Word("Triangle", "Driehoek", "fr"),
-        Word("Oiseau", "Vogel", "fr"),
-        Word("Fantome","Spook", "fr"),
-        Word("Nuit","Nacht", "fr"),
-        Word("Chaussures","Schoenen", "fr"),
-        Word("Terre","Aarde", "fr"),
-        Word("Prison","Gevangenis", "fr")
+        EnglishWord("Hello", "Hallo"),
+        EnglishWord("Today", "Vandaag"),
+        EnglishWord("Food", "Voedsel"),
+        EnglishWord("Triangle", "Driehoek"),
+        EnglishWord("Bird", "Vogel"),
+        EnglishWord("Ghost", "Spook"),
+        EnglishWord("Night", "Nacht"),
+        EnglishWord("Shoes", "Schoenen"),
+        EnglishWord("Earth", "Aarde"),
+        EnglishWord("Jail", "Gevangenis"),
+        FrenchWord("Bonjour", "Hallo"),
+        FrenchWord("Aujourd'hui", "Vandaag"),
+        FrenchWord("Nourriture", "Voedsel"),
+        FrenchWord("Triangle", "Driehoek"),
+        FrenchWord("Oiseau", "Vogel"),
+        FrenchWord("Fantome","Spook"),
+        FrenchWord("Nuit","Nacht"),
+        FrenchWord("Chaussures","Schoenen"),
+        FrenchWord("Terre","Aarde"),
+        FrenchWord("Prison","Gevangenis")
     )
 
     fun play(){
-
+        
 val filterLanguage = words.filter {
     it.language == language
 }
-
         val numberOfWords = roundSize;
         val randomWord = filterLanguage.shuffled().take(numberOfWords).toMutableSet()
 
         val selectedWord = randomWord.random()
         println("Amount of rounds : $roundSize")
-println("Chosen language : $language")
+        println("Chosen language : $language")
         while (randomWord.isNotEmpty()){
-
             println("What is the translation of ${selectedWord.original}")
             val userAnswer = readLine()
 
